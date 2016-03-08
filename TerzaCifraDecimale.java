49f504a09914af4301165dd449131fa3f0c1698b
0dr4n <0dr4n04l@gmail.com>
2016-03-08 19:02:09 +0100

    Update TerzaCifraDecimale.java

--- TerzaCifraDecimale.java
+++ TerzaCifraDecimale.java
@@ -1,5 +1,10 @@
+import java.util.*;
 public class TerzaCifraDecimale {
   public static void main(String[] args) {
-    
+    Scanner input = new Scanner(Sysmte.in);
+    int a;
+    System.out.println("Inserisci un valore per a: ");
+    a = input.nextInt();
+    System.out.println("Il doppi dii a è: "+a);
   ]
 }
